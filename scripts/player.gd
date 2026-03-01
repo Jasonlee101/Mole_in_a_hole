78eb8a2e27cdd2fd528f387dd40530a6e30e9ddb
Jason <leej70@kgv.hk>
2026-03-01 12:00:34 +0800

    sdf

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,23 +3,25 @@ extends CharacterBody2D
 const SPEED = 130.0
 const JUMP_VELOCITY = -300.0
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
-var dead = false 
+var dead = false
+var swinging = false 
 
 @onready var animated_sprite = $AnimatedSprite2D
 @onready var hand_pivot = $HandPivot
 @onready var pickaxe_sprite = $HandPivot/Pickaxe
 @onready var anim_player = $HandPivot/AnimationPlayer
+@onready var timer = $Timer
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity.y += gravity * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 	if Input.is_action_just_pressed("mine"):
-		pickaxe_sprite.play("idle")
+		pickaxe_sprite.play("swing")
+		timer.start()
 	# Get the input direction and handle the movement/deceleration.
 	var direction := Input.get_axis("move_left", "move_right")
 	
@@ -49,11 +51,12 @@ func _process(_delta):
 	hand_pivot.look_at(get_global_mouse_position())
 	# Keep the pickaxe upright when aiming left
 	var mouse_pos = get_global_mouse_position()
-	pickaxe_sprite.play("swing")
 	if mouse_pos.x < global_position.x:
 		hand_pivot.scale.y = -1
 	else:
 		hand_pivot.scale.y = 1
+		
 
-func swing_pickaxe():
-	$HandPivot/Pickaxe.play("swing")
+
+func _on_timer_timseout() -> void:
+	print("stop")
