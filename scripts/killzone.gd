8c9f27019871fbd0a07474e2857d7f95be07e34f
lima6-tech <lima6@kgv.hk>
2026-03-01 09:47:08 +0800

    updates

--- scripts/killzone.gd
+++ scripts/killzone.gd
@@ -3,8 +3,11 @@ extends Area2D
 @onready var timer = $Timer
 
 func _on_body_entered(body):
+		
 	Engine.time_scale = 0.5
 	body.get_node('CollisionShape2D').queue_free()
+	var anim = body.get_node("AnimatedSprite2D")
+	anim.play("death")  
 	timer.start()
 
 func _on_timer_timeout():
