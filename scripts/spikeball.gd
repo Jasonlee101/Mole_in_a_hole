4e661780e701467dee3658822d7731ca0e05b2ef
Jason <leej70@kgv.hk>
2026-03-15 13:50:06 +0800

    spikeball

---
+++ scripts/spikeball.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+
+@export_enum(
+	"UpDown", 
+	"LeftRight"
+	) var spikeball_type: String = "UpDown"
+const SPEED = 60
+
+var direction = -1
+@onready var ray_cast_up = $RayCastUp
+@onready var ray_cast_down = $RayCastDown
+@onready var animated_sprite = $AnimatedSprite2D
+
+func _process(delta):
+	if ray_cast_up.is_colliding():
+		direction = 1
+	if ray_cast_down.is_colliding():
+		direction = -1
+	if spikeball_type:
+		position.x += direction * SPEED * delta 
+	else:
+		position.x += direction * SPEED * delta 
